056014ab92eab2d68684cf96f94e381441e8b48a
ShrutiYC <99820040+ShrutiYC@users.noreply.github.com>
2024-02-19 13:16:22 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log("Shruti")
+const accountId = 11223344
+let accountEmail = "shruti@gmail.com"
+var accountPassword = "445566"
+accountCity = "Jaipur"
+let accountstate;
+//accountId = 2node test.js
+
+
+accountEmail = "sdfghj@gmail.com"
+accountPassword = "123456"
+accountCity = "Pune"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate]);
---
+++ test1.js
@@ -0,0 +1,8 @@
+"use scrpit";
+console.log(3 + 3);
+console.log("Shruti")
+let name = "Shruti"
+let age = 21
+let isLoggedIn = false
+let state = null 
+console.log(typeof age);
