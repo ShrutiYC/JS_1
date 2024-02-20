dbd119d94ce8d29e6ca93a5be2e17296a2190e1b
ShrutiYC <99820040+ShrutiYC@users.noreply.github.com>
2024-02-20 10:33:54 +0530

    Add files via upload

---
+++ conversion_operation.js
@@ -0,0 +1,31 @@
+let score = "null"
+let score_1 = "23"
+let scorce_2 = "123abd"
+
+let score_4 = null
+let score_5 = true
+let scorce_6 = false
+
+console.log(typeof score);
+console.log(typeof score_1);
+console.log(typeof score_2);
+
+console.log(typeof score_4);
+console.log(typeof score_5);
+console.log(typeof score_6);
+
+let valueInnumber = Number (score)
+let valueInnumber_1 = Number (score_1)
+let valueInnumber_2 = Number (scorce_2)
+
+let valueInnumber_4 = Number (scorce_4)
+let valueInnumber_5 = Number (scorce_5)
+let valueInnumber_6 = Number (scorce_6)
+
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+console.log(valueInnumber_1);
+console.log(valueInnumber_2);
+console.log(valueInnumber_4);
+console.log(valueInnumber_5);
+console.log(valueInnumber_6);
