c91d9d8c417d1e7c7a490febd10cb5f7c1d757c0
ShrutiYC <99820040+ShrutiYC@users.noreply.github.com>
2024-02-20 10:33:26 +0530

    Add files via upload

---
+++ string_conversion.js
@@ -0,0 +1,18 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "HelloMoto"
+let str2 = " How are you?"
+let str3 = str1 + str2
+console.log(str3);
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3s
+num1 = num2 = num3 = 2 + 2
+console.log(num1,num2,num3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
